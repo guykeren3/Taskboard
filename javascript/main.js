dad94efa05f19de317174be529886dfe443064ae
guykeren3 <guykeren3@gmail.com>
2017-03-05 12:05:03 +0200

    finished UI of Modal

--- javascript/main.js
+++ javascript/main.js
@@ -36,6 +36,24 @@ const listTemplate = `
   <div class="panel-footer"><button class="add-card-button">Add a card</button></div>
 `;
 
+// const editCardTemplate = `<div class="modal fade" tabindex="-1" role="dialog">
+//   <div class="modal-dialog" role="document">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+//         <h4 class="modal-title">Modal title</h4>
+//       </div>
+//       <div class="modal-body">
+//         <p>One fine body&hellip;</p>
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div><!-- /.modal-content -->
+//   </div><!-- /.modal-dialog -->
+// </div><!-- /.modal -->`;
+
 let addListButton = document.querySelector('#add-list');
 
 function createList(data) {
@@ -69,6 +87,7 @@ function createList(data) {
       ulInList.appendChild(liInUl);
     }
   }
+
   // console.info(newListSpan);
   handleListTitle(newListSpan);
 
