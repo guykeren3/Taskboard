5e9149ba52a054730c1b887500939efa533b1609
guykeren3 <guykeren3@gmail.com>
2017-05-08 15:52:37 +0300

    Revert "working on a bug regarding the add a list button"
    This reverts commit ecc5a7237afc15b98bcd6a464e88cb2a27cdfc5b.

--- javascript/main.js
+++ javascript/main.js
@@ -69,6 +69,7 @@ const VIEW = (function () {
   }
 
   function createList(list) {
+    // console.info('createList', list);
     let addListButton = document.querySelector('#add-list');
 //catching the main div to push the other divs into it
     const listParent = document.createElement('div');
@@ -86,7 +87,9 @@ const VIEW = (function () {
 
     let divWrapper = document.querySelector('.wrapper');
 
-    // divWrapper.insertBefore(listParent, addListButton);
+    if (typeof addListButton !== 'null') {
+    divWrapper.insertBefore(listParent, addListButton);
+    }
 
     // Handle clicks on list title
     let newListSpan = listParent.querySelector('.list-title');
